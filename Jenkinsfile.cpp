386f2fa5239e3305b3ff7807585588ec9f886171
aayush-shandilya <107453733+aayush-shandilya@users.noreply.github.com>
2024-03-07 20:06:56 +0530

    Create Jenkinsfile.cpp

---
+++ Jenkinsfile.cpp
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'g++ working.cpp -o temp'
+                 build job: 'PES1UG22CS804_1', wait: false
+                 echo 'Build by CS806 successful'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh 'cat working.cpp'
+                echo 'Test by CS806 successful'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+               
+                 echo 'Deploy by CS806 successful'
+            }
+        }
+    }
+
+    post {
+        failure {
+            
+                echo 'Pipeline Failed'
+          
+        }
+    }
+}
